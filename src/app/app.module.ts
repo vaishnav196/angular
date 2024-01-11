d532ada46580ff5f541c2bf4d4e6ef804c643ba1
vaishnav196 <vaish00721@gmail.com>
2024-01-11 22:53:47 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EmplistComponent } from './emplist/emplist.component';
 import { EmpldetailsComponent } from './empldetails/empldetails.component';
 import { FormsComponent } from './forms/forms.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
